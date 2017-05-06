b1b2de17a916ede704922b9dbf528e439aa87987
Kurt Telep <Kurt.Telep@emc.com>
2017-05-05 20:49:01 -0400

    Significant Updates, final revision for EMCWorld

--- tilt_cruncher.py
+++ tilt_cruncher.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask import make_response, current_app
 from flask_sslify import SSLify
 from flask_apscheduler import APScheduler
 from collections import defaultdict
@@ -10,19 +11,32 @@ import redis
 import timeit
 import logging
 import datetime
-
+from datetime import timedelta
+from functools import update_wrapper
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__, static_url_path='/static')
 
-job_schedule = 10
+job_schedule = 1
 
 class Config(object):
     JOBS = [
         {
             'id': 'per_devid_stats',
             'func': 'tilt_cruncher:per_devid_stats',
+            'trigger': 'interval',
+            'seconds': job_schedule
+            },
+        {
+            'id': 'industry',
+            'func': 'tilt_cruncher:industry_counts',
+            'trigger': 'interval',
+            'seconds': job_schedule
+            },
+        {
+            'id': 'hand_check',
+            'func': 'tilt_cruncher:hand_check',
             'trigger': 'interval',
             'seconds': job_schedule
             },
@@ -142,11 +156,109 @@ except redis.ConnectionError:
     print "Unable to connect to a Redis server, check environment"
     sys.exit(1)
 
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
+
 def timestamp():
     now = time.time()
     localtime = time.localtime(now)
     milliseconds = '%03d' % int((now - int(now)) * 1000)
     return int(time.strftime('%Y%m%d%H%M%S', localtime) + milliseconds)
+
+def hand_check():
+    """ Calculates the hands (left or right) """
+    time.sleep(0.25)
+    start_time = timeit.default_timer()
+    logging.info("Beginning hand check")
+
+    device_keys = r.keys('devidhistory:*Hand')
+   
+    hands = dict()
+    hands["Left"] = 0
+    hands["Right"] = 0
+
+    hand_data = r.mget(device_keys)
+    for hand_val in hand_data:
+        if hand_val == "Left":
+            hands["Left"] = hands["Left"] + 1
+        else:
+            hands["Right"] = hands["Right"] + 1
+
+    logging.info(hands)
+    r.set("hand:right:history", hands["Right"])
+    r.set("hand:left:history", hands["Left"])
+
+def industry_counts():
+  
+    ind_dict = defaultdict(list)
+
+    device_keys = r.keys('devidhistory:*industry*') 
+
+    # Sort out which keys are for each industry
+    industry_data = r.mget(device_keys)
+    for i in range(len(industry_data)):
+       if industry_data[i] == "":
+           continue
+       else:
+           devid = device_keys[i].split(':')[1]
+           ind_dict[industry_data[i]].append(devid) 
+ 
+    shake_variance_key = "devidhistory:%s:TiltLR:Variance"
+
+    for i in ind_dict.keys():
+       keys_to_ret = [shake_variance_key % (j,) for j in ind_dict[i]]
+       shake_vals = [float(x) for x in r.mget(keys_to_ret)]
+       shake_avg = sum(shake_vals)/len(shake_vals)
+       r.set("industry:%s:shakeavg" % (i,),shake_avg)
+       
+    
+def calc_variance(mylist):
+    
+    avg = sum(mylist)/float(len(mylist))
+    temp = 0
+    for i in range(len(mylist)):
+        temp += (mylist[i] - avg) * (mylist[i] - avg) 
+        return temp / len(mylist);
 
 def per_devid_stats():
     """ Calculate Min, Max, and Average values of the current active devids """
@@ -154,7 +266,7 @@ def per_devid_stats():
     start_time = timeit.default_timer()
     logging.info("Beginning average calculation")
 
-    average_vals = ['TiltFB','TiltLR','Direction','ReqSize']
+    average_vals = ['TiltFB','TiltLR','Direction']
     device_keys = [x.split(':')[-1] for x in r.keys('devid:*')]
 
     to_insert = dict()
@@ -165,14 +277,32 @@ def per_devid_stats():
             avg_key = "devidhistory:%s:%s:Average" % (key, val)
             min_key = "devidhistory:%s:%s:Min" % (key, val)
             max_key = "devidhistory:%s:%s:Max" % (key, val)
+            var_key = "devidhistory:%s:%s:Variance" % (key, val)   
+
             hand = "devidhistory:%s:Hand" % (key)
-               
+             
             # Need to cast our vals from string to floats
             values = [float(x) for x in r.zrange(val_key,0,-1)]
-            avg = sum(values)/float(len(values))
+            total = sum(values)
+            avg = total/float(len(values))
+            variance = None
+ 
+            # Some stats on the last 25 samples
+            if len(values) < 25:
+                variance = calc_variance(values)
+            else:
+                variance = calc_variance(values[-25:])
 
-            if "TiltLR" in val:  # Basic hand analysis.  
-                if avg < 0:  # Right Handheld
+            logging.info("Variance - %d" % (variance,))
+            to_insert[var_key] = variance     
+
+            if "TiltLR" in val:  # Basic hand analysis, based on last 25 data points
+                if len(values) < 25:   
+                    hand_avg = avg
+                else:
+                    hand_avg = sum(values[-25:] )/float(25)
+                logging.info("hand_avg - %d" % (hand_avg))
+                if hand_avg < 0:  # Right Handheld
                     to_insert[hand] = "Right"
                 else:
                     to_insert[hand] = "Left"
@@ -214,7 +344,7 @@ def dev_id_counts(hours):
 
         # no need to RE-count already counted time periods
         logging.info(r.exists("devidcount:%s" % (top_score)))
-        if not r.exists(top_score):
+        if not r.exists("devidcount:%s" % (top_score)):
 
             # Get Count between top and bottom
             count = r.zcount("devidlist", bottom_score, top_score)
@@ -227,13 +357,70 @@ def dev_id_counts(hours):
     elapsed = timeit.default_timer() - start_time
     logging.info("Calculated Devid Counts in %s" % (str(elapsed)))
 
+@app.route('/tilt_hour')
+@app.route('/tilt_hour/<hours>')
+@crossdomain('*')
+def tilt_hour(hours=None):
+    keys = r.keys('devidcount:*')
+ 
+    res = dict()
+    if keys:
+        keys.sort()
+        if hours:
+             return_keys = keys[-int(hours):]
+        else:
+             return_keys = keys
+
+        for i in return_keys:
+            res[i.split(':')[1]]=int(r.get(i))
+
+    return jsonify(data=res)
+
+
+@app.route('/industry_shake')
+@crossdomain('*')
+def industry_shake():
+    keys = r.keys('industry:*')
+
+    shake_vals = r.mget(keys)
+    industries = [x.split(':')[1] for x in keys]
+    
+    ret = dict(zip(industries,shake_vals))
+
+    return jsonify(data=ret)
+
 @app.route('/')
+@crossdomain('*')
 def index():
     return "Tilt-Cruncher!"
 
    
+@app.route('/active_variance')
+@app.route('/active_variance/<devid>')
+@crossdomain('*')
+def active_variance(devid=None):
+    device_keys = []
+
+    if devid:
+       device_keys.append(devid)
+    else:
+       device_keys = [x.split(':')[-1] for x in r.keys('devid:*')]
+
+    average_vals = ['TiltFB','TiltLR','Direction']
+
+    return_val = defaultdict(dict)
+
+    for key in device_keys:
+        for val in average_vals:
+            var_key = "devidhistory:%s:%s:Variance" % (key, val)
+            average = r.get(var_key)
+            return_val[key][val] = average
+    
+    return jsonify(data=return_val)
+
 @app.route('/active_averages')
 @app.route('/active_averages/<devid>')
+@crossdomain('*')
 def active_averages(devid=None):
     device_keys = []
 
@@ -242,7 +429,7 @@ def active_averages(devid=None):
     else:
        device_keys = [x.split(':')[-1] for x in r.keys('devid:*')]
 
-    average_vals = ['TiltFB','TiltLR','Direction','ReqSize']
+    average_vals = ['TiltFB','TiltLR','Direction']
 
     return_val = defaultdict(dict)
 
@@ -255,8 +442,30 @@ def active_averages(devid=None):
     
     return jsonify(data=return_val)
 
+@app.route('/tilt_server_stats')
+@crossdomain('*')
+def tilt_server_stats():
+    device_keys = r.keys('server:*')
+
+    total_processed = r.mget(device_keys)
+    ret = defaultdict(dict)
+
+    for i in range(len(device_keys)):
+        ret['servers'][device_keys[i]] = total_processed[i]
+
+    total = 0
+    device_keys = r.keys('devid:*')
+    for i in device_keys:
+        total = total + int(r.zcount(i,'-inf','inf'))
+
+    ret['active'] = total
+
+    return jsonify(data=ret)
+
+
 @app.route('/active_latest')
 @app.route('/active_latest/<devid>')
+@crossdomain('*')
 def active_latest(devid=None):
     device_keys = []
 
@@ -265,9 +474,9 @@ def active_latest(devid=None):
     else:
        device_keys = [x.split(':')[-1] for x in r.keys('devid:*')]
 
-    vals = ['TiltFB','TiltLR','Direction','ReqSize']
+    vals = ['TiltFB','TiltLR','Direction']
     static_vals = ['OS','altitude','latitude','longitude',
-                   'direction','industry']
+                   'industry']
 
     return_val = defaultdict(dict)
 
@@ -275,18 +484,28 @@ def active_latest(devid=None):
         for val in vals:
             val_key = "devidhistory:%s:%s:Values" % (key, val)
             data = r.zrevrangebyscore(val_key,"+inf","-inf")
-            return_val[key][val] = data[0]
+            if data:
+                return_val[key][val] = data[0]
+            else:
+                return_val[key][val] = None
 
         for val in static_vals:
             val_key = "devidhistory:%s:%s:Values" % (key, val)
             data = r.get(val_key)
             return_val[key][val] = data
-            
-        hand_key = "devidHistory:%s:Hand" % (key,)
-        data = r.get(hand_key)
-        return_val[key]['Hand'] = data
-    
+      
+        for val in ["Hand","TotalBW"]:
+           val_key = "devidhistory:%s:%s" % (key, val)
+           data = r.get(val_key)
+           if data:
+                return_val[key][val] = data
+           else:
+                return_val[key][val] = None
+
+   
     return jsonify(data=return_val)
+
+
 
 sslify = SSLify(app)
 
